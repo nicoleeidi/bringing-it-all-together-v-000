819f3eb96a33f7d5fbe13b0a93aaa07aad6f98ce
Nicole Eidi <nicoleeidi1@gmail.com>
2018-03-06 22:35:34 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,13 @@
 class Dog
   attr_accessor :id, :name, :breed
-  def initialize
+  def initialize (id:nil, name:, breed:)
+    @id=id
+    @name=name
+    @breed=breed 
   end
   def self.create_table
-  end 
+    
+  end
   def self.drop_table
-  end 
-end 
+  end
+end
