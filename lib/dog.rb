9b2922407db129ce089129258b858754e9f4d82a
Nicole Eidi <nicoleeidi1@gmail.com>
2018-03-07 00:08:10 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -12,14 +12,14 @@ class Dog
     DB[:conn].execute("DROP TABLE dogs")
   end
   def self.new_from_db(row)
-
+      binding.pry
       new_dog=self.new
       new_dog.id=row[0]
       new_dog.name=row[1]
       new_dog.breed=row[2]
       new_dog
 
-      # create a new Student object given a row from the database
+    
     end
     def self.find_by_name(name)
         DB[:conn].execute("SELECT * FROM dogs WHERE name= ? LIMIT 1", name).map do |row|
