32e77176ff2032ad62b2dc3b5b27e64a20f155c8
Nicole Eidi <nicoleeidi1@gmail.com>
2018-03-07 00:11:31 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -14,9 +14,9 @@ class Dog
   def self.new_from_db(row)
       binding.pry
       new_dog=self.new(id:row[0],name:row[1],breed:row[2])
-      new_dog.id=row[0]
-      new_dog.name=row[1]
-      new_dog.breed=row[2]
+      # new_dog.id=row[0]
+      # new_dog.name=row[1]
+      # new_dog.breed=row[2]
       new_dog
 
 
